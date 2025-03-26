4aa87d4bfcdaf682254ee2cefc65cf309ebb44ef
lpm0073 <lpm0073@gmail.com>
2025-03-26 12:29:38 -0600

    chore: prep for build

--- setup.py
+++ setup.py
@@ -1,24 +1,109 @@
 """Setup for smarter package."""
 
+import os
+import re
+from typing import Dict
+
 from setuptools import find_packages, setup
 
-from setup_utils import get_semantic_version  # pylint: disable=import-error
-from setup_utils import load_readme, load_requirements
-from smarter.__about__ import __author__, __author_email__, __license__, __url__
+
+MODULE_NAME = "smarter"
+HERE = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(HERE, MODULE_NAME))
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
+def load_version() -> Dict[str, str]:
+    """Parse the __version__.py file to extract the version without importing the package."""
+    version_file_path = os.path.join(PROJECT_ROOT, "__version__.py")
+    version: Dict[str, str] = {}
+    with open(version_file_path, encoding="utf-8") as f:
+        for line in f:
+            # Match lines like __version__ = "0.1.0"
+            match = re.match(r"^__(\w+)__\s*=\s*[\"'](.+?)[\"']$", line)
+            if match:
+                version[f"__{match.group(1)}__"] = match.group(2)  # Ensure keys like "__version__"
+    return version
+
+
+def load_about() -> Dict[str, str]:
+    """Parse the __about__.py file to extract metadata without importing the package."""
+    about: Dict[str, str] = {}
+    about_path = os.path.join(PROJECT_ROOT, "__about__.py")
+    with open(about_path, encoding="utf-8") as f:
+        for line in f:
+            # Match lines like __author__ = "Author Name"
+            match = re.match(r"^__(\w+)__\s*=\s*[\"'](.+?)[\"']$", line)
+            if match:
+                about[f"__{match.group(1)}__"] = match.group(2)
+    return about
+
+
+VERSION = load_version()
+print("VERSION:", VERSION)
+
+
+def get_semantic_version() -> str:
+    """
+    Return the semantic version number.
+
+    Example valid values of __version__.py are:
+    0.1.17
+    0.1.17-next.1
+    0.1.17-next.2
+    0.1.17-next.123456
+    0.1.17-next-major.1
+    0.1.17-next-major.2
+    0.1.17-next-major.123456
+
+    Note:
+    - pypi does not allow semantic version numbers to contain a dash.
+    - pypi does not allow semantic version numbers to contain a 'v' prefix.
+    - pypi does not allow semantic version numbers to contain a 'next' suffix.
+    """
+    version = VERSION["__version__"]
+    version = re.sub(r"-next\.\d+", "", version)
+    return re.sub(r"-next-major\.\d+", "", version)
+
+
+def load_readme() -> str:
+    """Stringify the README."""
+    with open(os.path.join(HERE, "README.md"), encoding="utf8") as f:
+        readme = f.read()
+    # Replace img src for publication on pypi
+    return readme.replace(
+        "./doc/",
+        "https://github.com/lpm0073/secure-logger/raw/main/doc/",
+    )
+
+
+def load_requirements(filename) -> list:
+    with open(filename, encoding="utf-8") as f:
+        lines = f.read().splitlines()
+    # Filter out comments and empty lines
+    requirements = [line for line in lines if line and not line.startswith("#")]
+    return requirements
+
+
+README = load_readme()
+ABOUT = load_about()
+
+print("Current working directory:", os.getcwd())
+print("Detected packages:", find_packages(exclude=["smarter.tests"]))
 setup(
     name="smarter-api",
     version=get_semantic_version(),
     description="The official Python library for the Smarter platform Api.",
-    long_description=load_readme(),
+    long_description=README,
     long_description_content_type="text/markdown",
-    author=__author__,
-    author_email=__author_email__,
-    maintainer=__author__,
-    maintainer_email=__author_email__,
-    url=__url__,
-    license=__license__,
+    author=ABOUT["__author__"],
+    author_email=ABOUT["__author_email__"],
+    maintainer=ABOUT["__author__"],
+    maintainer_email=ABOUT["__author_email__"],
+    url=ABOUT["__url__"],
+    license=ABOUT["__license__"],
     license_files=("LICENSE.txt",),
     platforms=["any"],
     packages=find_packages(exclude=["tests", "tests.*"]),
